0d5d05661afabf458e49d5466cdf2d6f24ae6dfe
Andrew Szucs <andrew0szucs@gmail.com>
2017-12-04 12:05:24 +0100

    Solved 2nd part of Day 4 challenge

--- day04/day04.js
+++ day04/day04.js
@@ -7,25 +7,67 @@ const rl = readline.createInterface({
 })
 
 const state = {
-  validityOfPasses: []
+  validityOfPassesPart1: [],
+  validityOfPassesPart2: [],
 }
 
 rl.on('line', (line) => {
   const elements = line
                     .split(' ')
 
-  state.validityOfPasses.push(part1(elements))
+  state.validityOfPassesPart1.push(part1(elements))
+  state.validityOfPassesPart2.push(part2(elements))
 })
 
 rl.on('close', () => {
-  const numberOfValidPasses = state.validityOfPasses.reduce((acc, curr) => (curr) ? (acc += 1) : (acc), 0)
+  const numberOfValidPassesPart1 = state.validityOfPassesPart1.reduce((acc, curr) => (curr) ? (acc += 1) : (acc), 0)
+  const numberOfValidPassesPart2 = state.validityOfPassesPart2.reduce((acc, curr) => (curr) ? (acc += 1) : (acc), 0)
 
-  console.log('The number of valid passphrases: ', numberOfValidPasses)
+  console.log('The number of valid passphrases (Part 1): ', numberOfValidPassesPart1)
+  console.log('The number of valid passphrases (Part 2): ', numberOfValidPassesPart2)
   console.log('done')
 })
+
+// Part 1
+/*
+  To ensure security, a valid passphrase must contain no duplicate words.
+
+  For example:
+
+  aa bb cc dd ee is valid.
+  aa bb cc dd aa is not valid - the word aa appears more than once.
+  aa bb cc dd aaa is valid - aa and aaa count as different words.
+  The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+*/
 
 const part1 = input => {
   const uniqueElements = Array.from(new Set(input))
 
   return input.length === uniqueElements.length
+}
+
+// Part 2
+/*
+  Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+  For example:
+
+  abcde fghij is a valid passphrase.
+  abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+  a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+  iiii oiii ooii oooi oooo is valid.
+  oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+  Under this new system policy, how many passphrases are valid?
+*/
+
+const part2 = input => {
+
+  const sortedInput = input
+                        .map(word => Array.from(word))
+                        .map(word => word.sort())
+                        .map(word => word.join())
+
+  const uniqueInput = Array.from(new Set(sortedInput))
+
+  return sortedInput.length === uniqueInput.length
 }
