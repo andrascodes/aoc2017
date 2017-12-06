50b2c0628c4866ee65db6b0e7f5d6846789e34f8
Andrew Szucs <andrew0szucs@gmail.com>
2017-12-06 17:49:32 +0100

    Solved Day 6 challenge

--- day06/day06.js
+++ day06/day06.js
@@ -1,13 +1,86 @@
 const util = require('util');
 const fs = require('fs');
 
+const part1 = memoryBanks => {
+
+  const previousConfigs = []
+
+  while(!previousConfigs.includes(memoryBanks.join())) {
+    
+    previousConfigs.push(memoryBanks.join())
+
+    let indexOfBankWithMostBlocks = memoryBanks.reduce((maxIndex, currentValue, currentIndex, array) => {
+      if(currentValue > array[maxIndex]) {
+        maxIndex = currentIndex
+      }
+      return maxIndex
+    }, 0)
+
+    let index = indexOfBankWithMostBlocks
+    let blocksToBeRedistributed = memoryBanks[indexOfBankWithMostBlocks]
+    memoryBanks[indexOfBankWithMostBlocks] = 0
+    while(blocksToBeRedistributed > 0) {
+      index++
+      if(memoryBanks[index] === undefined) {
+        index = 0
+      }
+      memoryBanks[index]++
+      blocksToBeRedistributed--
+    }
+  }
+
+  return previousConfigs.length
+
+}
+
+const part2 = memoryBanks => {
+  
+    const previousConfigs = []
+    
+    while(!previousConfigs.includes(memoryBanks.join())) {
+
+      previousConfigs.push(memoryBanks.join())
+  
+      let indexOfBankWithMostBlocks = memoryBanks.reduce((maxIndex, currentValue, currentIndex, array) => {
+        if(currentValue > array[maxIndex]) {
+          maxIndex = currentIndex
+        }
+        return maxIndex
+      }, 0)
+  
+      let index = indexOfBankWithMostBlocks
+      let blocksToBeRedistributed = memoryBanks[indexOfBankWithMostBlocks]
+      memoryBanks[indexOfBankWithMostBlocks] = 0
+      while(blocksToBeRedistributed > 0) {
+        index++
+        if(memoryBanks[index] === undefined) {
+          index = 0
+        }
+        memoryBanks[index]++
+        blocksToBeRedistributed--
+      }
+    }
+    
+    const repeatingConfig = memoryBanks.join()
+    const indexOfFirstOccuranceOfRepeatingConfig = previousConfigs.findIndex(value => {
+      return value === repeatingConfig
+    })
+
+    return previousConfigs.length - indexOfFirstOccuranceOfRepeatingConfig
+  
+  }
+
 const main = async () => {
 
   const readFile = util.promisify(fs.readFile)
   try {
-    const file = await readFile("day06/test.txt", 'utf8')
+    const file = await readFile("day06/input.txt", 'utf8')
     const input = file.split('\t').map(str => Number(str))
-    console.log(input)
+
+    const part1Input = input.slice(0)
+    const part2Input = input.slice(0)
+    console.log('Solution for Part 1: ', part1(part1Input))
+    console.log('Solution for Part 2: ', part2(part2Input))
   }
   catch(error) {
     console.error(error)
